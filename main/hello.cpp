e21ef13c70596e4ee33433a6ac2ed4770ea0036e
dheerajcl <cldheeraj541@gmail.com>
2025-03-11 15:51:52 +0530

    Added another cout statement

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "Say my Name!!\n";
+  cout << "Goddamn right!!\n"; 
   return 0;
 }
